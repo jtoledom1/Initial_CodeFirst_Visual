40f22174ad8d2d8a3bc6ab5d9d20cc761a63385c
José Toledo Muñoz <JtoledoM1@outlook.com>
2023-03-08 08:22:37 -0600

    Elimimnado de Form1 agregado de  carpeta data y clase customer

---
+++ Data/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P203.Data
+{
+    class Customer
+    {
+
+        #region properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+        #endregion
+
+
+
+
+    }
+}
