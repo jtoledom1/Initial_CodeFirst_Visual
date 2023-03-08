5118fa4be12331a0b987e81b1dd7f0fab8d34150
José Toledo Muñoz <JtoledoM1@outlook.com>
2023-03-08 09:08:35 -0600

    Corrección de errores en AppConfig

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace P203.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<P203.Data.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(P203.Data.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
