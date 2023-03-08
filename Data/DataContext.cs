d817e84c8c4892887106deb6084e6fda0ca6b23e
José Toledo Muñoz <JtoledoM1@outlook.com>
2023-03-08 08:46:07 -0600

    Clase DataContext Hereda de DbContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P203.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("name=con")
+        {
+
+            
+        }
+        
+
+        
+    }
+}
